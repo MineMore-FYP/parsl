c4f61a306ca16d2553737054cb80a2a444edc6bf
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2019-11-08 19:43:44 +0530

    Adding parsl to transformation folder done

--- transformation/categorise.py
+++ transformation/categorise.py
@@ -1,6 +1,13 @@
 #categorise
 import pandas as pd
 import numpy as np
+
+import parsl
+from parsl import load, python_app
+from parsl.configs.local_threads import config
+
+load(config)
+
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -11,9 +18,13 @@ import userScript
 #done on integers to categorise into ranges
 
 
-df = pd.read_csv(sys.argv[1])
+df = pd.read_csv("/home/kalpani/Documents/FYP/testcsv/test.csv")
 
-#user defined number of categories
-numberOfCategories=3
+@python_app
+def categorize():
+	#user defined number of categories
+	numberOfCategories=3
 
-pd.cut(np.array([1, 7, 5, 4, 6, 3]), numberOfCategories)
+	pd.cut(np.array([1, 7, 5, 4, 6, 3]), numberOfCategories)
+
+categorize()
