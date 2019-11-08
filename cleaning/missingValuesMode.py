2bb6c0abb32828920a535b8811cb29c13556d319
rajiniw95 <rajiniw95@gmail.com>
2019-11-08 17:11:42 +0530

    missing values MODE : cleaning module

--- cleaning/missingValuesMode.py
+++ cleaning/missingValuesMode.py
@@ -1,37 +1,53 @@
-import pandas as pd
-import numpy as np
-import statistics
+from parsl import load, python_app
+from parsl.configs.local_threads import config
+load(config)
 
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+@python_app
+def missingValuesMode():
+    import pandas as pd
+    import numpy as np
+    import statistics
+    from statistics import mode, StatisticsError
 
-import dataType
-import userScript
+    import os,sys,inspect
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parentdir = os.path.dirname(currentdir)
+    sys.path.insert(0,parentdir)
 
-df = pd.read_csv(userScript.outputDataset)
-#drop unique columns
-numOfRows = df.shape[0]
+    import dataType
+    import userScript
 
-for col in df.columns:
-    if len(df[col].unique()) == numOfRows:
-        df.drop(col,inplace=True,axis=1)
+    df = pd.read_csv(userScript.inputDataset)
+    #drop unique columns
+    numOfRows = df.shape[0]
 
-if(userScript.modeColumns == "all"):
-    #Mode of all columns
-    colNames = list(df)
-else:
-    #Mode of user defined columns
-    colNames = userScript.modeColumns
+    for col in df.columns:
+        if len(df[col].unique()) == numOfRows:
+            df.drop(col,inplace=True,axis=1)
 
+    if(userScript.modeColumns == "all"):
+        #Mode of all columns
+        colNames = list(df)
+    else:
+        #Mode of user defined columns
+        colNames = userScript.modeColumns
 
-df2 = df
-df1 = pd.DataFrame()
-for col in colNames:
+    df2 = df
+    df1 = pd.DataFrame()
+    
+    for col in colNames:
 
-	df1 = df[col].dropna()
-	modeOfCol = statistics.mode(df1)
-	df2[col].fillna(modeOfCol, inplace = True)
+        try:
+            df1 = df[col].dropna()
+            modeOfCol = statistics.mode(df1)
+            df2[col].fillna(modeOfCol, inplace = True)
+        except StatisticsError:
+            print(col)
+            print ("No unique mode found")
 
-df2.to_csv(userScript.outputDataset, index = False, header=True)
+    df2.to_csv("/home/rajini/FYP/testcsv/missingValuesModeOUTPUT.csv", index = False, header=True)
+
+    ret  = "Missing values mode complete"
+    return ret
+
+print(missingValuesMode().result())
