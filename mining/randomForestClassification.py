8d616d9a4692ae41d7254c39a358b9373afac322
amandaw95 <amanda_wijewickrama@yahoo.com>
2019-11-22 11:02:12 +0530

    RF changes

--- mining/randomForestClassification.py
+++ mining/randomForestClassification.py
@@ -4,12 +4,30 @@ load(config)
 
 import pandas as pd
 import numpy as np
+import time
+
 
 
 df = pd.read_csv("/home/amanda/Downloads/bill_authentication.csv")
 
+from parsl.config import Config
+from parsl.executors.threads import ThreadPoolExecutor
+
+maxThreads = 10
+local_threads = Config(
+    executors=[
+        ThreadPoolExecutor(
+            max_threads=maxThreads,
+            label='local_threads'
+        )
+    ]
+)
+
+
+
+
 @python_app
-def rfClassifier():
+def rfClassifier(estimators):
 
 	import pandas as pd
 	import numpy as np
@@ -37,19 +55,19 @@ def rfClassifier():
 
 	from sklearn.ensemble import RandomForestClassifier
 
-	classifier = RandomForestClassifier(n_estimators=20, random_state=0)
+	classifier = RandomForestClassifier(n_estimators=estimators, random_state=0)
 	classifier.fit(X_train, y_train)
 	y_pred = classifier.predict(X_test)
 
 	from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-	print(confusion_matrix(y_test,y_pred))
-	print(classification_report(y_test,y_pred))
-	print(accuracy_score(y_test, y_pred))
+	#print(confusion_matrix(y_test,y_pred))
+	#print(classification_report(y_test,y_pred))
+	#print(accuracy_score(y_test, y_pred))
 
-	return "done"
+	return str(confusion_matrix(y_test,y_pred)) + '\n' +(classification_report(y_test,y_pred)) + '\n' + str(accuracy_score(y_test, y_pred))
 
 
 
-print(rfClassifier().result())
+#print(rfClassifier().result())
 
--- userScript.py
+++ userScript.py
@@ -19,12 +19,12 @@ waitTime = 10000
 #SELECTION
 #select columns
 #if "all" select everything. else give a list ["whatever1", "whatever2"]
-selectColumns = ["GLOBALEVENTID","SQLDATE", "Year", "Actor2Code", "Actor2Name", "Actor2Religion1Code", "Actor2Type1Code", "EventCode", "EventRootCode", "QuadClass", "GoldsteinScale", "NumMentions", "NumSources", "NumArticles", "AvgTone", "Actor2Geo_FullName", "SOURCEURL"]
+selectColumns = ["SQLDATE", "Actor1Geo_Type", "Actor1Geo_CountryCode", "QuadClass", "GoldsteinScale", "NumMentions", "AvgTone"]
 
 #select rows
 selectFromRow = OrderedDict()
-selectFromRow['Year'] = ["2018", "2019"]
-selectFromRow['Actor1Name'] = ["DUTCH"]
+#selectFromRow['Year'] = ["2018", "2019"]
+#selectFromRow['Actor1Name'] = ["DUTCH"]
 
 #CLEANING
 #Drop unique columns
@@ -35,15 +35,15 @@ missingValues = ["n/a", "na", "--"]
 
 #Drop user defined cols
 #user defined column array
-dropColumns = ["SQLDATE", "SOURCEURL"]
+#dropColumns = ["SQLDATE", "SOURCEURL"]
 
 #drop columns according to user defined empty value percentage
-userDefinedColPercentage = 30
+userDefinedColPercentage = 20
 
 #drop user defined rows
-dropFromRow = OrderedDict()
-dropFromRow['Actor2Name'] = ["LONDON", "DUTCH"]
-dropFromRow['EventCode'] = ["40"]
+#dropFromRow = OrderedDict()
+#dropFromRow['Actor2Name'] = ["LONDON", "DUTCH"]
+#dropFromRow['EventCode'] = ["40"]
 
 #drop rows according to user defined empty value percentage
 userDefinedRowPercentage = 20
@@ -52,7 +52,7 @@ userDefinedRowPercentage = 20
 
 #missing value interpolation
 #all int and float columns interpolated
-interpolateColumns = "all" #if interpolateColumns = "all", all int columns, else give a list
+#interpolateColumns = "all" #if interpolateColumns = "all", all int columns, else give a list
 
 #mode for user defined columns
 #all the rest left from interpolate, mode
@@ -65,21 +65,21 @@ modeColumns = "all"
 
 #transformation
 #Standardize
-userDefinedStandardizeColumns = ["AvgTone"] #standadizeColumns = "all", all int columns, else give a list
+#userDefinedStandardizeColumns = ["AvgTone"] #standadizeColumns = "all", all int columns, else give a list
 
 #rescale
 userDefinedRescaleColumns = OrderedDict()
 #0 = lowerBoundry, 100 = upperBoundry
 userDefinedRescaleColumns["NumMentions"] = [0, 100]
-userDefinedRescaleColumns["NumSources"] = [0, 100]
-userDefinedRescaleColumns["NumArticles"] = [0, 100]
+#userDefinedRescaleColumns["NumSources"] = [0, 100]
+#userDefinedRescaleColumns["NumArticles"] = [0, 100]
 
 #binarize
 userDefinedBinarizeColumns = OrderedDict()
 #0 = lowerBoundry, 100 = upperBoundry
-userDefinedBinarizeColumns["NumMentions"] = [10.0]
-userDefinedBinarizeColumns["NumSources"] = [10.0]
-userDefinedBinarizeColumns["NumArticles"] = [10.0]
+#userDefinedBinarizeColumns["NumMentions"] = [10.0]
+#userDefinedBinarizeColumns["NumSources"] = [10.0]
+#userDefinedBinarizeColumns["NumArticles"] = [10.0]
 
 
 #encoding - gives an automatical numerical value to string categorical columns
@@ -96,19 +96,19 @@ userDefinedEncodeColumns = ["Actor2Code", "Actor2Name", "Actor2Geo_FullName"]
 
 #kmeans
 #selectedColumns1 = selectColumns - dropColumns
-numberOfClusters = 3
+#numberOfClusters = 3
 
-startWithNumberOfClusters = 2
-endWithNumberOfClusters = 8
+#startWithNumberOfClusters = 2
+#endWithNumberOfClusters = 8
 #input   location
-plotLocation = "/home/amanda/FYP/testcsv"
+#plotLocation = "/home/amanda/FYP/testcsv"
 
 
 #KNN
 #define targetColumn
-targetColumnName = "QuadClass"
-targetColumn = ["QuadClass"]
-numberOfneighbours = 5
+#targetColumnName = "QuadClass"
+#targetColumn = ["QuadClass"]
+#numberOfneighbours = 5
 
 #linear regression
 #selectedColumns2 = selectColumns - dropColumns
