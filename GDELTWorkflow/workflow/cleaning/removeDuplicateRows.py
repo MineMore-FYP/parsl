e80a5023c0d6da66dd02037046ad6488dda8fbd5
amandaw95 <amanda_wijewickrama@yahoo.com>
2019-11-26 17:27:18 +0530

    remove duplicate rows

--- GDELTWorkflow/workflow/cleaning/removeDuplicateRows.py
+++ GDELTWorkflow/workflow/cleaning/removeDuplicateRows.py
@@ -28,7 +28,7 @@ def removeDuplicateRows(startRowIndex, endRowIndex, dFrame):
 
 
 #read csv with defined missing values
-df = pd.read_csv("/home/amanda/FYP/testcsv/RFout.csv", index_col = False)
+df = pd.read_csv("/home/amanda/FYP/testcsv/test1.csv", index_col = False)
 numOfRows = df.shape[0]
 print(numOfRows)
 
@@ -36,9 +36,12 @@ print(numOfRows)
 dfNew = pd.DataFrame()
 maxThreads = threadconfig.maxThreads
 
+results = []
+
 #not parallel --> relatively small number of rows here
 if numOfRows <= maxThreads:
-	df1 = (removeDuplicateRows(0, i+1, df).result())
+	df1 = removeDuplicateRows(0, numOfRows, df)
+	results.append(df1)
 
 #parallel
 elif numOfRows > maxThreads:
@@ -46,25 +49,44 @@ elif numOfRows > maxThreads:
 	if (numOfRows % maxThreads == 0):
 		eachThreadRows = numOfRows / maxThreads 
 		for i in range (maxThreads):
-			df1 = (removeDuplicateRows(i,(i+eachThreadRows),df).result())
-			dfNew = pd.concat([dfNew, df1] , axis=0)
+			df1 = removeDuplicateRows(i,(i+eachThreadRows),df)
+			results.append(df1)
+			#dfNew = pd.concat([dfNew, df1] , axis=0)
 		
 	else:
 		eachThreadRows = numOfRows // (maxThreads-1)
 		for i in range (0,(maxThreads-1)*eachThreadRows, eachThreadRows):
 			print ("i", i)
 			print("i+eachThreadRows", (i+eachThreadRows))
-			df1 = (removeDuplicateRows(i, (i+eachThreadRows), df).result())
-			dfNew = pd.concat([dfNew, df1], axis=0)
+			df1 = removeDuplicateRows(i, (i+eachThreadRows), df)
+			results.append(df1)
+			#dfNew = pd.concat([dfNew, df1], axis=0)
 
 		print("last thread",(eachThreadRows * (maxThreads-1))	)
-		df2 = (removeDuplicateRows((eachThreadRows * (maxThreads-1)), numOfRows, df).result())
+		df2 = removeDuplicateRows((eachThreadRows * (maxThreads-1)), numOfRows, df)
+		results.append(df2)
 	
-		dfNew = pd.concat([dfNew, df2] , axis=0)
+		#dfNew = pd.concat([dfNew, df2] , axis=0)
 
 
+# wait for all apps to complete
+#[r.result() for r in results]
 
+
+newlist = []	
+for i in results:
+	newlist.append(i.result())
+
+for i in newlist:
+	
+	dfNew = pd.concat([dfNew, i], axis=0)
+
+
+#dfNew = newlist[0]
+print(dfNew)
 
 dfNew.drop("index",inplace=True,axis=1)
+print(dfNew)
+
 dfNew.to_csv ("/home/amanda/FYP/testcsv/removeDuplicateRowsOUTPUT.csv", index = False, header=True)
 #print(removeDuplicateRows(50,100,df).result())
--- GDELTWorkflow/workflow/threadconfig.py
+++ GDELTWorkflow/workflow/threadconfig.py
@@ -1,7 +1,7 @@
 from parsl.config import Config
 from parsl.executors.threads import ThreadPoolExecutor
 
-maxThreads = 4
+maxThreads = 8
 local_threads = Config(
     executors=[
         ThreadPoolExecutor(
