9c3dd6a340c5dd827c42ba7b4656d5f25a28435a
amandaw95 <amanda_wijewickrama@yahoo.com>
2019-11-26 18:00:12 +0530

    missing mode module

--- GDELTWorkflow/workflow/cleaning/missingValuesMode.py
+++ GDELTWorkflow/workflow/cleaning/missingValuesMode.py
@@ -18,7 +18,7 @@ import userScript
 import threadconfig
 
 colsToMode = userScript.modeColumns
-df = pd.read_csv("/home/amanda/FYP/testcsv/test.csv")
+df = pd.read_csv("/home/amanda/FYP/testcsv/test1.csv")
 
 @python_app
 def missingValuesMode(startColIndex, endColIndex, dFrame, colsMode):
@@ -68,11 +68,16 @@ print(numOfCols)
 lasThreadCols = 0
 dfNew = pd.DataFrame()
 
+results = []
+
 #one col per thread
 if numOfCols <= maxThreads:
 	for i in range (numOfCols):
-		df1 = (missingValuesMode(0, i+1, df, colsToMode).result())
-		dfNew = pd.concat([dfNew, df1] , axis=1)
+		print("test1")
+		df1 = missingValuesMode(i, i+1, df, colsToMode)
+		results.append(df1)
+		#dfNew = pd.concat([dfNew, df1] , axis=1)
+
 
 
 elif numOfCols > maxThreads:
@@ -80,8 +85,9 @@ elif numOfCols > maxThreads:
 	if (numOfCols % maxThreads == 0):
 		eachThreadCols = numOfCols / maxThreads 
 		for i in range (maxThreads):
-			df1 = (missingValuesMode(i,(i+eachThreadCols),df,colsToMode).result())
-			dfNew = pd.concat([dfNew, df1] , axis=1)
+			df1 = missingValuesMode(i,(i+eachThreadCols),df,colsToMode)
+			#dfNew = pd.concat([dfNew, df1] , axis=1)
+			results.append(df1)
 		
 	else:
 		eachThreadCols = numOfCols // (maxThreads-1)
@@ -89,17 +95,25 @@ elif numOfCols > maxThreads:
 		for i in range (0,(maxThreads-1)*eachThreadCols, eachThreadCols):
 			print ("i", i)
 			print("i+eachThreadCols", (i+eachThreadCols))
-			df1 = (missingValuesMode(i,(i+eachThreadCols),df,colsToMode).result())
-			dfNew = pd.concat([dfNew, df1], axis=1)
+			df1 = missingValuesMode(i,(i+eachThreadCols),df,colsToMode)
+			#dfNew = pd.concat([dfNew, df1], axis=1)
+			results.append(df1)
 
 		print("last thread",(eachThreadCols * (maxThreads-1))	)
-		df2 = (missingValuesMode((eachThreadCols * (maxThreads-1)),numOfCols,df,colsToMode).result())
+		df2 = missingValuesMode((eachThreadCols * (maxThreads-1)),numOfCols,df,colsToMode)
+		results.append(df2)
+		#dfNew = pd.concat([dfNew, df2] , axis=1)
+
+
+
+
+newlist = []	
+for i in results:
+	newlist.append(i.result())
+
+for i in newlist:
 	
-		dfNew = pd.concat([dfNew, df2] , axis=1)
-
-
-
-
+	dfNew = pd.concat([dfNew, i], axis=1)
 
 print(dfNew)
 dfNew.to_csv ("/home/amanda/FYP/testcsv/mode.csv", index = False, header=True)
