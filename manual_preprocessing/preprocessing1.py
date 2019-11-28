6616bd74e633b300bcf18958791c297147be6cfb
rajiniw95 <rajiniw95@gmail.com>
2019-11-28 23:57:50 +0530

    generate sql date

--- manual_preprocessing/preprocessing1.py
+++ manual_preprocessing/preprocessing1.py
@@ -17,6 +17,9 @@ dfMonth=df.loc[:][1]
 dfDate=df.loc[:][2]
 dfCountry=df.loc[:][3]
 
+
+# SEPERATE DATAFRAMES BY COUNTRY
+
 # determine unique countries; used to make seperate dataframe per country
 for i in df.loc[:][3].unique():
 	#print("dfCountry"+i)
@@ -33,6 +36,9 @@ for i in df.loc[:][3].unique():
 	# write each country dataframe to csv file. (in seperate folder)
 	dfCountry.to_csv("/home/rajini/Desktop/riots/countryDF/dfCountry"+i+".csv", sep=',', encoding='utf-8', index=False, header=False)
 
+
+# INSERT NEW RECORD FOR EVERY DAY IN THE YEAR
+
 loc = "/home/rajini/Desktop/riots/countryDF/"
 
 list_of_df = []
@@ -44,15 +50,6 @@ for f in os.listdir(loc):
 		df = pd.read_csv(loc+f,  sep = ',', header=None)
 		# append dataframe to a list
 		list_of_df.append(df)
-
-#print(list_of_df[0])
-#print(list_of_df[1])
-
-'''
-for df in list_of_df:
-    print(df)
-    print("--")
-'''
 
 ThirtyDays = [4,6,9,11]
 ThirtyOneDays = [1,3,5,7,8,10,12]
@@ -85,6 +82,9 @@ for df in list_of_df:
 	
 	#print(df)
 
+
+# REMOVE DUPLICATE ROWS 
+
 loc2 = "/home/rajini/Desktop/riots/filledDF/"
 
 list_of_filled_df = []
@@ -101,12 +101,57 @@ for f in os.listdir(loc2):
 
 # handle duplicate zero records
 for df in list_of_filled_df:
+	# get country name for dataframe from the first row of df 		
 	countryName = df.loc[0][3]
+
 	# Select all duplicate rows based on multiple columns
 	# leave the first record (showing riot) and delete the next
 	# For the subset argument, specify the first n-1 columns
 	df = df.drop_duplicates(subset=df.columns[:-1], keep='first')	
 	df.to_csv("/home/rajini/Desktop/riots/removeDuplicateDF/dfRemoveDuplicate"+countryName+".csv", sep=',', encoding='utf-8', index=False, header=False)
+
+
+# CONCATENATE COLUMNS TO GET SQL DATE
+
+loc3 = "/home/rajini/Desktop/riots/removeDuplicateDF/"
+
+list_of_duplicate_removed_df = []
+
+# run loop for all files in previously created dataframe folder
+for f in os.listdir(loc3):
+	if f.endswith(".csv"):
+		#print(f)
+		df = pd.read_csv(loc3+f,  sep = ',', header=None)
+		# append dataframe to a list
+		list_of_duplicate_removed_df.append(df)
+		#print(list_of_filled_df)
+
+for df in list_of_duplicate_removed_df:
+	# get country name for dataframe from the first row of df 		
+	countryName = df.loc[0][3]
+
+	# convert float to int	
+	for i in range (3):
+		df[i]=df[i].astype(int)
+	#print(df.dtypes)
+
+	# convert int to string
+	for i in range (3):
+		df[i]=df[i].astype(str)
+	#print(df.dtypes)
+	
+	# obtain SQLDATE in column 1
+	df[0] = df[0]+df[1]+df[2]
+
+	# drop month and date columns
+	df = df.drop([1, 2], axis=1)
+
+	print(df)
+	
+	df.to_csv("/home/rajini/Desktop/riots/sqldateDF/dfsqldate"+countryName+".csv", sep=',', encoding='utf-8', index=False, header=False)
+
+
+##### 	ISSSUE TO RESOLVE : SQL DATE FOR ONE DIGIT NUMBERS
 
 #print(list_of_filled_df[0])
 #print(list_of_filled_df[1])
