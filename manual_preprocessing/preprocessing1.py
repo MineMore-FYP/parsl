62d5d56b938f981e9d5228ea6533489e5e7082c7
rajiniw95 <rajiniw95@gmail.com>
2019-11-29 11:48:00 +0530

    integrate results

--- manual_preprocessing/preprocessing1.py
+++ manual_preprocessing/preprocessing1.py
@@ -156,31 +156,48 @@ for df in list_of_duplicate_removed_df:
 	# drop month and date columns
 	df = df.drop([1, 2], axis=1)
 
+	#df.columns = ['SQLDATE', 'ActorGeo_CountryCode', 'Indicator']
+
 	#print(df)
 	
 	df.to_csv("/home/rajini/Desktop/riots/sqldateDF/dfsqldate"+countryName+".csv", sep=',', encoding='utf-8', index=False, header=False)
+ 
+
+# COMBINE ALL DATAFRAMES
+
+loc4 = "/home/rajini/Desktop/riots/sqldateDF/"
+os.chdir(loc4)
+
+allFiles = os.listdir(loc4)
+
+# CSV file selection
+
+selectedFiles = []
+
+for filename in allFiles:
+    	selectedFiles.append(filename)
+
+#print(selectedFiles)
+
+# Create new CSV file to write all CSV files generated from previous step 
+with open("/home/rajini/Desktop/riots/combinedRiots.csv", "w", newline='', encoding="utf8") as outcsv:
+	writer = csv.writer(outcsv, delimiter=',')
+
+	# write the header
+	writer.writerow(["SQLDATE", "ActorGeo_CountryCode", "Indicator"]) 
+	
+	# write the actual content line by line
+	for filename in selectedFiles:
+		with open(filename, 'r', newline='', encoding="utf8") as incsv:
+			reader = csv.reader(incsv, delimiter=',')
+			writer.writerows(row for row in reader)
+'''
 
 #print(list_of_filled_df[0])
 #print(list_of_filled_df[1])
 
 #df.to_csv("/home/rajini/Desktop/riots/datapp.csv", sep=',', encoding='utf-8', index=False, header=False)
- 
-#ISSSUE TO RESOLVE : SQL DATE FOR ONE DIGIT NUMBERS
-
-'''
-df.to_csv (sys.argv[1], index = False, header=True)
-
-'''
-'''
-with open("/home/amanda/FYP/ds/combined.csv", "w", newline='', encoding="utf8") as outcsv:
-    writer = csv.writer(outcsv, delimiter=',')
-    writer.writerow(header) # write the header
 
 
-    # write the actual content line by line
-    for filename in selectedFiles:
-        with open(filename, 'r', newline='', encoding="utf8") as incsv:
-            reader = csv.reader(incsv, delimiter='\t')
-            writer.writerows(row + [0.0] for row in reader)
 '''
 
