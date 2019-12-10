28afdcf960641a7687908f6a3f4a9f3305831394
amandaw95 <amanda_wijewickrama@yahoo.com>
2019-12-10 22:34:06 +0530

    Fixed error with combined dataset

--- GDELTWorkflow/test.go
+++ GDELTWorkflow/test.go
@@ -15,10 +15,19 @@ import (
 
 func pythonCall(progName string, inChannel chan <- string) {
 	cmd := exec.Command("python3", progName)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	out, err := cmd.CombinedOutput()
 	log.Println(cmd.Run())
-	msg := ("Module Completed: " + progName)
+
+	if err != nil {
+		fmt.Println(err)
+		// Exit with status 3.
+    os.Exit(3)
+	}
+	fmt.Println(string(out))
+	msg := string(out)[:len(out)-1]
+	//msg := ("Module Completed: " + progName)
 	inChannel <- msg
 }
 
@@ -32,6 +41,7 @@ func main(){
 	//check if input location is available
 	cmd := exec.Command("python", "-c", "from workflow import userScript; print userScript.inputDataset")
 	out, err := cmd.CombinedOutput()
+
 	if err != nil {
 		fmt.Println(err)
 		// Exit with status 3.
@@ -54,7 +64,7 @@ func main(){
 	pythonCall("workflow/cleaning/dropUniqueColumns.py", outChannelModule1)
 	messagePassing(outChannelModule1, outChannelModule2)
 
-	fmt.Println("jskdfkjdh")
-	fmt.Println(outChannelModule2)
+	//fmt.Println("jskdfkjdh")
+	fmt.Println(<- outChannelModule2)
 
 }
