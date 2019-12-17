f33bbef677daccf338076e7c9f102197e3e817a5
mpiuser <rajiniw95@gmail.com>
2019-12-17 17:01:59 +0530

    fix loc

--- GDELTWorkflow/workflow/transformation/normalize.py
+++ GDELTWorkflow/workflow/transformation/normalize.py
@@ -12,19 +12,20 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
 import userScript
-import threadconfig
+#import threadconfig
 import dataType
 
+df = pd.read_csv(userScript.outputLocation + "missingValuesMode.csv")
+outputDataset = userScript.outputLocation + "normalize.csv"
 
-df = pd.read_csv("/home/amanda/FYP/testcsv/test.csv")
 colsToNormalize = userScript.userDefinedNormalizeColumns
 
 for i in colsToNormalize:
 	if dataType.dataType(i, df) == "str":
-		print("Cannot normalize string column: ", i)
+		#print("Cannot normalize string column: ", i)
 		colsToNormalize.remove(i)
 	
-print(colsToNormalize)
+#print(colsToNormalize)
 
 @python_app
 def normalize(startColIndex, endColIndex, dFrame, normalizeCols):
@@ -37,15 +38,14 @@ def normalize(startColIndex, endColIndex, dFrame, normalizeCols):
 
 	dfColNames = list(df)
 	normalizeCols = list(set(dfColNames).intersection(normalizeCols))
-	print(normalizeCols)
+	#print(normalizeCols)
 
 	if len(normalizeCols)!=0:
-
 
 		# Normalize The Column
 		# Create x, where x the 'scores' column's values as floats
 		x = df[normalizeCols].values.astype(float)
-		print(x)
+		#print(x)
 
 		# Create a minimum and maximum processor object
 		min_max_scaler = preprocessing.MinMaxScaler()
@@ -61,17 +61,15 @@ def normalize(startColIndex, endColIndex, dFrame, normalizeCols):
 			j=j+1
 	return df
 
-
 #print(normalize(0,3,df,['AvgTone', 'QuadClass']).result())
 #this returns the column(s) that was normalized. 
 #drop the previous column and concat at the end.
 #parallelize the number of cols in the user script instruction - change for mode, normalize, encode
 
-
-maxThreads = threadconfig.maxThreads
+maxThreads = 4
 
 numOfCols = df.shape[1]
-print(numOfCols)
+#print(numOfCols)
 
 lasThreadCols = 0
 dfNew = pd.DataFrame()
@@ -88,7 +86,7 @@ if numOfCols <= maxThreads:
 
 
 elif numOfCols > maxThreads:
-	print("test2")
+	#print("test2")
 	if (numOfCols % maxThreads == 0):
 		eachThreadCols = numOfCols / maxThreads 
 		for i in range (maxThreads):
@@ -100,13 +98,13 @@ elif numOfCols > maxThreads:
 		eachThreadCols = numOfCols // (maxThreads-1)
 		lasThreadCols = numOfCols % (maxThreads-1)
 		for i in range (0,(maxThreads-1)*eachThreadCols, eachThreadCols):
-			print ("i", i)
-			print("i+eachThreadCols", (i+eachThreadCols))
+			#print ("i", i)
+			#print("i+eachThreadCols", (i+eachThreadCols))
 			df1 = normalize(i,(i+eachThreadCols),df,colsToNormalize)
 			#dfNew = pd.concat([dfNew, df1], axis=1)
 			results.append(df1)
 
-		print("last thread",(eachThreadCols * (maxThreads-1))	)
+		#print("last thread",(eachThreadCols * (maxThreads-1))	)
 		df2 = normalize((eachThreadCols * (maxThreads-1)),numOfCols,df,colsToNormalize)
 		results.append(df1)
 		#dfNew = pd.concat([dfNew, df2] , axis=1)
@@ -120,9 +118,9 @@ for i in newlist:
 	
 	dfNew = pd.concat([dfNew, i], axis=1)
 
+#print(dfNew)
+dfNew.to_csv (outputDataset, index = False, header=True)
 
-
-print(dfNew)
-dfNew.to_csv ("/home/amanda/FYP/testcsv/normalize.csv", index = False, header=True)
+print("Module Completed: Normalize")
 
 
