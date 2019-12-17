b65fbd6e6296ab00a15c5f160d63b215b6e8e664
mpiuser <rajiniw95@gmail.com>
2019-12-17 17:05:28 +0530

    fix loc

--- GDELTWorkflow/workflow/transformation/encoding.py
+++ GDELTWorkflow/workflow/transformation/encoding.py
@@ -17,9 +17,11 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
 import userScript
-import threadconfig
+#import threadconfig
 
-df = pd.read_csv("/home/amanda/FYP/testcsv/RFout.csv")
+df = pd.read_csv(userScript.outputLocation + "splitIntoRows.csv")
+outputDataset = userScript.outputLocation + "encoding.csv"
+
 encodeCols = userScript.userDefinedEncodeColumns
 
 @python_app
@@ -33,8 +35,7 @@ def encode(startColIndex, endColIndex, dFrame, encodeCols):
 
 	dfColNames = list(df)
 	encodeCols = list(set(dfColNames).intersection(encodeCols))
-	print(encodeCols)
-
+	#print(encodeCols)
 
 	if len(encodeCols) != 0:
 		x = df[encodeCols].astype(str)
@@ -55,13 +56,10 @@ def encode(startColIndex, endColIndex, dFrame, encodeCols):
 
 #print(encode(0,7, df, encodeCols).result())
 
-
-
-
-maxThreads = threadconfig.maxThreads
+maxThreads = 4
 
 numOfCols = df.shape[1]
-print(numOfCols)
+#print(numOfCols)
 
 lasThreadCols = 0
 dfNew = pd.DataFrame()
@@ -76,9 +74,8 @@ if numOfCols <= maxThreads:
 		#print (df1)
 		#dfNew = pd.concat([dfNew, df1] , axis=1)
 
-
 elif numOfCols > maxThreads:
-	print("test2")
+	#print("test2")
 	if (numOfCols % maxThreads == 0):
 		eachThreadCols = numOfCols / maxThreads 
 		for i in range (maxThreads):
@@ -90,29 +87,25 @@ elif numOfCols > maxThreads:
 		eachThreadCols = numOfCols // (maxThreads-1)
 		lasThreadCols = numOfCols % (maxThreads-1)
 		for i in range (0,(maxThreads-1)*eachThreadCols, eachThreadCols):
-			print ("i", i)
-			print("i+eachThreadCols", (i+eachThreadCols))
+			#print ("i", i)
+			#print("i+eachThreadCols", (i+eachThreadCols))
 			df1 = encode(i,(i+eachThreadCols),df,encodeCols)
 			#dfNew = pd.concat([dfNew, df1], axis=1)
 			results.append(df1)
 
-		print("last thread",(eachThreadCols * (maxThreads-1))	)
+		#print("last thread",(eachThreadCols * (maxThreads-1))	)
 		df2 = encode((eachThreadCols * (maxThreads-1)),numOfCols,df,encodeCols)
 		results.append(df1)
 		#dfNew = pd.concat([dfNew, df2] , axis=1)
-
 
 newlist = []	
 for i in results:
 	newlist.append(i.result())
 
 for i in newlist:
-	
 	dfNew = pd.concat([dfNew, i], axis=1)
 
-
-
-print(dfNew)
+#print(dfNew)
 dfNew.to_csv ("/home/amanda/FYP/testcsv/encode.csv", index = False, header=True)
 
 
