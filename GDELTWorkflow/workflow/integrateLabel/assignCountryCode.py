03f50fc39018b2e1c4ca5cc973e55f8d110d113a
mpiuser <rajiniw95@gmail.com>
2019-12-20 13:08:17 +0530

    fic acled module for user script input

--- GDELTWorkflow/workflow/integrateLabel/assignCountryCode.py
+++ GDELTWorkflow/workflow/integrateLabel/assignCountryCode.py
@@ -6,13 +6,38 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-import userScript2
-#df = userScript.inputDataFrame
+import userScript
 
-df = pd.read_csv(userScript2.outputLocation + "addLabelColumn.csv")
-outputDataset = userScript2.outputLocation + "assignCountryCode.csv"
+currentModule = "assignCountryCode"
 
-country=userScript2.country
+workflowNumber = sys.argv[1]
+
+if workflowNumber == "1":
+	orderOfModules = userScript.orderOfModules1
+	inputDataset = userScript.inputDataset1
+	outputLocation = userScript.outputLocation1
+	country=userScript.country1
+elif workflowNumber == "2":
+	orderOfModules = userScript.orderOfModules2
+	inputDataset = userScript.inputDataset2
+	outputLocation = userScript.outputLocation2
+	country=userScript.country2
+	
+
+df = pd.DataFrame()
+for i in range(len(orderOfModules)):
+	#print(orderOfModules[i])
+	if currentModule == orderOfModules[i]:
+		if i == 0:
+			df = pd.read_csv(inputDataset)
+			break
+		else:
+			previousModule = orderOfModules[i-1]
+			df = pd.read_csv(outputLocation + previousModule + ".csv")
+			break
+
+outputDataset = outputLocation + currentModule + ".csv"
+
 
 def assignCountryCode(df1, c):
 	df = df1
--- GDELTWorkflow/workflow/integrateLabel/splitDate.py
+++ GDELTWorkflow/workflow/integrateLabel/splitDate.py
@@ -6,11 +6,35 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-import userScript2
-#df = userScript.inputDataFrame
+import userScript
 
-df = pd.read_csv(userScript2.outputLocation + "assignCountryCode.csv")
-outputDataset = userScript2.outputLocation + "splitDate.csv"
+currentModule = "splitDate"
+
+workflowNumber = sys.argv[1]
+
+if workflowNumber == "1":
+	orderOfModules = userScript.orderOfModules1
+	inputDataset = userScript.inputDataset1
+	outputLocation = userScript.outputLocation1
+elif workflowNumber == "2":
+	orderOfModules = userScript.orderOfModules2
+	inputDataset = userScript.inputDataset2
+	outputLocation = userScript.outputLocation2
+	
+
+df = pd.DataFrame()
+for i in range(len(orderOfModules)):
+	#print(orderOfModules[i])
+	if currentModule == orderOfModules[i]:
+		if i == 0:
+			df = pd.read_csv(inputDataset)
+			break
+		else:
+			previousModule = orderOfModules[i-1]
+			df = pd.read_csv(outputLocation + previousModule + ".csv")
+			break
+
+outputDataset = outputLocation + currentModule + ".csv"
 
 def assignCountryCode(df1):
 	df = df1
