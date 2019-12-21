13d7750298995b2d500ba7600a413e1a915643e8
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2019-12-21 16:42:26 +0530

    Kmean model training, testing and hpo done

--- kmeans/python1.py
+++ kmeans/python1.py
@@ -1,7 +1,7 @@
-import pandas as pd
+#import pandas as pd
 
-df1 = pd.read_csv("/home/amanda/FYP/testcsv/out.csv",engine = 'python')
-header = list(df1)
+#df1 = pd.read_csv("/home/amanda/FYP/testcsv/out.csv",engine = 'python')
+#header = list(df1)
 numberOfClusters = [2,3,4,5,6,7]
 
-loc = "/home/amanda/Documents/FYP/kmeansplots"
+#loc = "/home/amanda/Documents/FYP/kmeansplots"
