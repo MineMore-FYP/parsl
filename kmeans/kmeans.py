2d2ac06f887eded8eda334b48bd41617b615e6af
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2019-12-23 17:32:24 +0530

    Take accuracy and cluster pairs as struct

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -1,14 +1,14 @@
-#import pandas as pd
-#from pandas import DataFrame
+import pandas as pd
+from pandas import DataFrame
 #from sklearn.cluster import KMeans
-#import numpy as np
+import numpy as np
 #from sklearn.model_selection import train_test_split
 #from sklearn.cluster import KMeans
 #from sklearn.metrics import accuracy_score
 
 #import matplotlib.pyplot as plt
 #from matplotlib.backends.backend_pdf import PdfPages
-
+import sys
 import parsl
 from parsl import load, python_app
 from parsl.configs.local_threads import config
@@ -18,7 +18,7 @@ load(config)
 
 from python1 import *
 
-
+Iteration_no = sys.argv[1]
 @python_app
 def kmeans(n):	
 	import pandas as pd
@@ -38,30 +38,25 @@ def kmeans(n):
 	X_train, X_test,y_train,y_test =  train_test_split(X,y,test_size=0.20,random_state=70)
 	k_means = KMeans(n_clusters=n)
 	kmeans = k_means.fit(X_train)
-	#centroids = kmeans.cluster_centers_
-	#print("x axis: "+i + " , y axis: " + j)
-	#print(centroids)
-	#print("\n")
-	#print("Training set")
-	#print(X_train)
-	print(k_means.labels_[:])
-	print(y_train[:])
+
+	#print(k_means.labels_[:])
+	#print(y_train[:])
 
 
 
 	k_means.predict(X_test)
-	#print("\nTesting set")
-	#print(X_test)
-	print(k_means.labels_[:])
-	print(y_test[:])
+	
+	#print(k_means.labels_[:])
+	#print(y_test[:])
 
 	score = accuracy_score(y_test,k_means.predict(X_test))
-	print('Accuracy:{0:f}'.format(score) + ' For ' + str(n) + ' clusters.\n' )
+	#print('Accuracy:{0:f}'.format(score) + ' For ' + str(n) + ' clusters.\n' )
 				
 				
-	#strng = 'kmeans done for ' + str(n) + ' clusters' --v.reshape(-1, 1)
+	#.reshape(-1, 1)
 	#pass all the input parameters and the score
-	return [n,score]		    						  			    	
+	ClusterNo_accuracy = [n,score]
+	return ClusterNo_accuracy	    						  			    	
 					    						    	
 
 results = []
@@ -70,10 +65,16 @@ for i in numberOfClusters:
 	results.append(app_future)
 	
 # print each job status, initially all are running
-print ("Job Status: {}".format([r.done() for r in results]))
+#print ("Job Status: {}".format([r.done() for r in results]))
 
 # wait for all apps to complete
 return_array = [r.result() for r in results]
 
+dfa=pd.DataFrame(return_array)
+dfa.columns = ["No_of_clusters", "Accuracy"]
+print(dfa)
+
+dfa.to_csv (r'/home/mpiuser/Documents/FYP/gdelt/kmeans/' + Iteration_no + '_kmeans.csv', index = None, header=True)
+
 # print each job status, they will now be finished
-print ("Job Status: {}".format(return_array))
+#print ("Job Status: {}".format(return_array))
--- kmeans/testx.go
+++ kmeans/testx.go
@@ -6,38 +6,100 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	//"strconv"
-
-	"io/ioutil"
-	//"log"
+	"io"
+	//"io/ioutil"
+	"log"
 	//"time"
 	"strconv"
+	//"reflect"
+	"path/filepath"
+	"bufio"
+    	"encoding/csv"
+	"encoding/json"
+	//"io.ReadSeeker"
 )
 
 
-func simplePythonCall(progName string) [][]string {
-	cmd := exec.Command("python3", progName)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os. Stderr
-	if cmd.Stderr != nil { fmt.Println(cmd.Stderr); }
-	//log.Println(cmd.Run())
-	Accuaracy_array,_ := ioutil.ReadAll(cmd.Stdout)
-	fmt.Println(Accuaracy_array)
-	return Accuaracy_array
+func simplePythonCall(progName string, itr string) string{
+	cmd := exec.Command("python3", progName, itr)
+	out, err := cmd.CombinedOutput()
+    	if err != nil { fmt.Println(err); }
+    	//fmt.Println(reflect.TypeOf(out))
+	return string(out)
 }
 
+func removeIt(ss Accuracy_class, ssSlice []Accuracy_class) []Accuracy_class {
+    for idx, v := range ssSlice {
+        if v == ss {
+            return append(ssSlice[0:idx], ssSlice[idx+1:]...)
+        }
+    }
+    return ssSlice
+}
+
+type Accuracy_class struct {
+    Clusters string `json:"clusters"`
+    Accuracy string `json:"accuracy"`
+}
 
 func main(){
-	accuracy_array = [][]string
+	
 	for i := 1;  i<=10; i++ {
-		//cmd := exec.Command("python3", "import kmeans; print kmeans.return_array")
-    		//fmt.Println(cmd.Args)
-    		//out, err := cmd.CombinedOutput()
-    		//if err != nil { fmt.Println(err); }
-    			//fmt.Println(string(out))
-		//kmeans
-		accuracy_array = simplePythonCall("kmeans.py")
-		fmt.Println(accuracy_array)
-                fmt.Printf("Kmeans with clusters 2,3,4,5,6,7 ran for " + strconv.Itoa(i) + " time(s).")
+		
+		var x = simplePythonCall("kmeans.py", strconv.Itoa(i))
+		fmt.Println(x)
+		
+                fmt.Printf("Kmeans with clusters 2,3,4,5,6,7 ran for " + strconv.Itoa(i) + " time(s).\n")
         }
+
+	var files []string
+
+    	root := "/home/mpiuser/Documents/FYP/gdelt/kmeans/"
+    	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        	files = append(files, path)
+        	return nil
+    	})
+    	if err != nil {
+        	panic(err)
+    	}
+
+	var Accuracy_set []Accuracy_class
+
+    	for _, file := range files {
+        	//if directory ignore
+		fi, err := os.Stat(file)
+		    if err != nil {
+			fmt.Println(err)
+			return
+		    }
+		     
+		    var mode = fi.Mode(); 
+		    if mode.IsDir() == true {
+			continue
+		    }			
+
+
+		csvFile, _ := os.Open(file)
+		
+    		reader := csv.NewReader(bufio.NewReader(csvFile))
+		
+		    for {
+			line, error := reader.Read()
+			if error == io.EOF {
+			    break
+			} else if error != nil {
+			    log.Fatal(error)
+			}
+			Accuracy_set = append(Accuracy_set, Accuracy_class{
+			    Clusters: line[0],
+			    Accuracy: line[1],
+			})
+		    }
+		    
+		    Accuracy_set = removeIt(Accuracy_class{"No_of_clusters", "Accuracy"}, Accuracy_set)
+		    
+    	}
+	accuracyJson, _ := json.Marshal(Accuracy_set)
+	fmt.Println(string(accuracyJson))
+	
 }
