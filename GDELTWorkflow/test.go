6f2f6d13db79ba6be0f0e461a4bddf186a613835
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2019-12-24 13:28:58 +0530

    Kmeans iterations,model training and max accuracy to main wf

--- GDELTWorkflow/test.go
+++ GDELTWorkflow/test.go
@@ -50,12 +50,52 @@ func integratePythonCall(progName string, inChannel1 chan <- string, inChannel2 
 }
 
 
-func simplePythonCall(progName string){
+func simplePythonCall1(progName string){
 	cmd := exec.Command("python3", progName)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os. Stderr
 	log.Println(cmd.Run())
 }
+//========================functions for kmeans===================
+func simplePythonCall(progName string, itr string) string{
+	cmd := exec.Command("python3", progName, itr)
+	out, err := cmd.CombinedOutput()
+    	if err != nil { fmt.Println(err); }
+    	//fmt.Println(reflect.TypeOf(out))
+	return string(out)
+}
+
+func removeIt(ss Accuracy_class, ssSlice []Accuracy_class) []Accuracy_class {
+    for idx, v := range ssSlice {
+        if v == ss {
+            return append(ssSlice[0:idx], ssSlice[idx+1:]...)
+        }
+    }
+    return ssSlice
+}
+
+type Accuracy_class struct {
+    Clusters int64 `json:"clusters"`
+    Accuracy float64 `json:"accuracy"`
+}
+
+func FindMaxAccuracy(Accuracy_set []Accuracy_class) (max Accuracy_class) {
+	
+	max = Accuracy_set[0]
+	for _, accuracy_obj := range Accuracy_set {
+		if accuracy_obj.Accuracy > max.Accuracy {
+			max = accuracy_obj
+		}
+	}
+	return max
+}
+
+func Display(accuracy_obj Accuracy_class) {
+	fmt.Println("No of clusters: ", accuracy_obj.Clusters)
+	fmt.Println("Accuracy: ", accuracy_obj.Accuracy)
+}
+
+///=====================================================
 
 func messagePassing(inChannel <- chan string, outChannel chan <- string ){
 	msg := <- inChannel
@@ -147,7 +187,7 @@ func main(){
 	fmt.Println(commandsArray)
 
 	//configurations
-	simplePythonCall("workflow/parslConfig.py")
+	simplePythonCall1("workflow/parslConfig.py")
 
 
 	//start module execution from here onwards
@@ -278,7 +318,78 @@ func main(){
 
 }
 
+/*
+	//==========================For kmeans - include in main loop========================
+	for i := 1;  i<=10; i++ {
+		
+		var x = simplePythonCall("kmeansModelTraining.py", strconv.Itoa(i))
+		fmt.Println(x)
+		
+                fmt.Printf("Kmeans with clusters 2,3,4,5,6,7 ran for " + strconv.Itoa(i) + " time(s).\n")
+        }
 
+	var files []string
+
+    	root := "/home/mpiuser/Documents/FYP/gdelt/kmeans/"
+    	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        	files = append(files, path)
+        	return nil
+    	})
+    	if err != nil {
+        	panic(err)
+    	}
+
+	var Accuracy_set []Accuracy_class
+
+    	for _, file := range files {
+        	//if directory ignore
+		fi, err := os.Stat(file)
+		    if err != nil {
+			fmt.Println(err)
+			return
+		    }
+		     
+		    var mode = fi.Mode(); 
+		    if mode.IsDir() == true {
+			continue
+		    }			
+
+
+		csvFile, _ := os.Open(file)
+		
+    		reader := csv.NewReader(bufio.NewReader(csvFile))
+		
+		    for {
+			line, error := reader.Read()
+			if error == io.EOF {
+			    break
+			} else if error != nil {
+			    log.Fatal(error)
+			}
+			var clusters int64
+			var accuracy float64
+			clusters, _ = strconv.ParseInt(line[0],10,0)
+			accuracy, _ = strconv.ParseFloat(line[1],64)
+			//fmt.Println(reflect.TypeOf(newc))
+			Accuracy_set = append(Accuracy_set, Accuracy_class{
+			    Clusters: clusters,
+			    Accuracy: accuracy,
+			})
+		    }
+		    
+		    //Accuracy_set = removeIt(Accuracy_class{"No_of_clusters", "Accuracy"}, Accuracy_set)
+		    
+    	}
+	//accuracyJson, _ := json.Marshal(Accuracy_set)
+	//fmt.Println(string(accuracyJson))
+	var max = FindMaxAccuracy(Accuracy_set)
+	Display(max)
+	
+	var optimalClusters = strconv.FormatInt(max.Clusters, 10)
+	fmt.Println(reflect.TypeOf(optimalClusters))
+	simplePythonCall("knowledage_presentation.py", "3")
+
+*/
 
 /*
 
